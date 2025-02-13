05dc590741ddd554afe561816e9805d196ee55f9
wardenscotland <macnamara.charlie@gmail.com>
2025-02-13 21:51:05 +0000

    light mode default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,mdx}'],
-  darkMode: 'class',
+  darkMode: 'media',
   plugins: [require('@tailwindcss/typography')],
   future: {
     hoverOnlyWhenSupported: true,
